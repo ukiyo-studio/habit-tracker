347d221c2af615bc0395d22fb805979bb5446cf2
ukiyo-studio <ukiyo.development@gmail.com>
2025-01-12 20:52:35 +0000

    setup eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,36 @@
-// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-native', '@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": "warn",
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'prettier/prettier': ['warn', { singleQuote: true, tabWidth: 2 }],
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': ['off'],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
