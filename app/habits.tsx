db5d2838502bbdd6c075146d25c2b3f649b3710b
ukiyo-studio <ukiyo.development@gmail.com>
2025-01-15 20:41:19 +0000

    Update habits.tsx

--- app/habits.tsx
+++ app/habits.tsx
@@ -1,7 +1,13 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 
 export function HabitsScreen() {
   return (
-    
+    <View>
+      <View>
+        <TextInput placeholder='Your course goal!'></TextInput>
+        <Button title='Add Goal'></Button>
+        <Text>List of Goals...</Text>
+      </View>
+    </View>
   );
 }
